87e45b2bdc13601d09e775c25c95712e50f65154
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-01 17:37:27 -0400

    feat: :sparkles: add function to validate and get calories from inputs

--- script.js
+++ script.js
@@ -69,3 +69,20 @@ function calculateCalories(e) {
 
   output.classList.remove('hide');
 }
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
+  }
+  return calories;
+}
