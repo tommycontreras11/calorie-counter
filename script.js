1dbef20d8d1200bfb26bff19853e3d8240637c82
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-01 17:38:29 -0400

    feat: :sparkles: add event listeners to buttons

--- script.js
+++ script.js
@@ -98,3 +98,7 @@ function clearForm() {
   output.innerText = '';
   output.classList.add('hide');
 }
+
+addEntryButton.addEventListener("click", addEntry);
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener('click', clearForm);
